fe29e46b736ee40c2eb71a26830583b5c3b377ff
Zhang <y.zhang-13@utwente.nl>
2023-01-18 16:57:25 +0100

    initialization

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Yi Zhang
+@contact: zhangyi_aero@hotmail.com
+@time: 11/26/2022 2:56 PM
+"""
+
+import os
+absolute_path = os.path.dirname(__file__)
+import sys
+if absolute_path not in sys.path:
+    sys.path.append(absolute_path)
+
+import src.config as config
---
+++ src/config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Global variables .
+
+@author: Yi Zhang
+@contact: zhangyi_aero@hotmail.com
+@time: 11/26/2022 2:56 PM
+"""
+
+_global_variables = {
+    'space_dim': 3,
+}
+
+
+def set_space_dim(ndim):
+    """"""
+    _global_variables['space_dim'] = ndim
+
+
+def get_space_dim():
+    """"""
+    return _global_variables['space_dim']
