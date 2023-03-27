0a49c7415a71334431fbe98d9de18fbdd63d5572
mathischeap <zhangyi_aero@hotmail.com>
2023-03-27 11:05:03 +0200

    updates

--- src/wf/ap.py
+++ src/wf/ap.py
@@ -210,66 +210,67 @@ if __name__ == '__main__':
 
     oph = samples.pde_canonical_pH(n=3, p=3)[0]
     a3, b2 = oph.unknowns
+    oph.pr()
 
     wf = oph.test_with(oph.unknowns, sym_repr=[r'v^3', r'u^2'])
     wf = wf.derive.integration_by_parts('1-1')
-    # wf.pr(indexing=True)
+    wf.pr(indexing=True)
 
     td = wf.td
     td.set_time_sequence()  # initialize a time sequence
 
     td.define_abstract_time_instants('k-1', 'k-1/2', 'k')
     # td.differentiate('0-0', 'k-1', 'k')
-    td.average('0-1', b2, ['k-1', 'k'])
+    # td.average('0-1', b2, ['k-1', 'k'])
 
     # td.differentiate('1-0', 'k-1', 'k')
-    td.average('1-1', a3, ['k-1', 'k'])
-    td.average('1-2', a3, ['k-1/2'])
+    # td.average('1-1', a3, ['k-1', 'k'])
+    # td.average('1-2', a3, ['k-1/2'])
     dt = td.time_sequence.make_time_interval('k-1', 'k')
 
     wf = td()
 
-    # wf.pr()
+    wf.pr()
 
     wf.unknowns = [
-        a3 @ td.time_sequence['k'],
-        b2 @ td.time_sequence['k'],
+        a3,
+        b2,
     ]
 
-    wf = wf.derive.split(
-        '0-0', 'f0',
-        [a3 @ td.ts['k'], a3 @ td.ts['k-1']],
-        ['+', '-'],
-        factors=[1/dt, 1/dt],
-    )
+    # wf = wf.derive.split(
+    #     '0-0', 'f0',
+    #     [a3 @ td.ts['k'], a3 @ td.ts['k-1']],
+    #     ['+', '-'],
+    #     factors=[1/dt, 1/dt],
+    # )
 
-    wf = wf.derive.split(
-        '0-2', 'f0',
-        [ph.d(b2 @ td.ts['k-1']), ph.d(b2 @ td.ts['k'])],
-        ['+', '+'],
-        factors=[1/2, 1/2],
-    )
+    # wf = wf.derive.split(
+    #     '0-1', 'f0',
+    #     [ph.d(b2 @ td.ts['k-1']), ph.d(b2 @ td.ts['k'])],
+    #     ['+', '+'],
+    #     factors=[1/2, 1/2],
+    # )
 
-    wf = wf.derive.split(
-        '1-0', 'f0',
-        [b2 @ td.ts['k'], b2 @ td.ts['k-1']],
-        ['+', '-'],
-        factors=[1/dt, 1/dt]
-    )
+    # wf = wf.derive.split(
+    #     '1-0', 'f0',
+    #     [b2 @ td.ts['k'], b2 @ td.ts['k-1']],
+    #     ['+', '-'],
+    #     factors=[1/dt, 1/dt]
+    # )
 
-    wf = wf.derive.split(
-        '1-2', 'f0',
-        [a3 @ td.ts['k-1'], a3 @ td.ts['k']],
-        ['+', '+'],
-        factors=[1/2, 1/2],
-    )
+    # wf = wf.derive.split(
+    #     '1-1', 'f0',
+    #     [a3 @ td.ts['k-1'], a3 @ td.ts['k']],
+    #     ['+', '+'],
+    #     factors=[1/2, 1/2],
+    # )
 
-    wf = wf.derive.rearrange(
-        {
-            0: '0, 3 = 1, 2',
-            1: '3, 0 = 2, 1, 4',
-        }
-    )
+    # wf = wf.derive.rearrange(
+    #     {
+    #         0: '0, 3 = 1, 2',
+    #         1: '3, 0 = 2, 1, 4',
+    #     }
+    # )
 
     ph.space.finite(3)
 
@@ -277,11 +278,11 @@ if __name__ == '__main__':
 
     # (a3 @ td.ts['k']).ap(r"\vec{\alpha}")
 
-    # wf.pr()
+    wf.pr()
 
     ap = wf.ap()
 
-    mp = ap.mp()
+    # mp = ap.mp()
 
     ap.pr()
     # print(wf.unknowns, wf.test_forms)
